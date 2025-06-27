6bcc6cfbf1dccb9f447467ae1133e31e06019815
ejblaza <ejblaza@gmail.com>
2025-06-27 02:56:21 -0700

    Finished Project

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 var numOne = 0;
 var numTwo = 0;
+var result = 0;
+var opCheck = false;
 
-// testing creation of git
+var rs = require("readline-sync");
+
+var operator = rs.question(
+  'What operation would you like to perform? "+", "-", "*", "/", or "%": '
+);
+
+if (
+  operator == "+" ||
+  operator == "-" ||
+  operator == "*" ||
+  operator == "/" ||
+  operator == "%"
+) {
+  opCheck = true;
+} else {
+  while (!opCheck) {
+    operator = rs.question('Invalid input! "+", "-", "*", "/", or "%":  ');
+    if (
+      operator == "+" ||
+      operator == "-" ||
+      operator == "*" ||
+      operator == "/" ||
+      operator == "%"
+    ) {
+      opCheck = true;
+    }
+  }
+}
+
+numOne = rs.questionInt("Please enter the first number: ");
+numTwo = rs.questionInt("Please enter the second number: ");
+
+switch (operator) {
+  case "+":
+    result = numOne + numTwo;
+    break;
+  case "-":
+    result = numOne - numTwo;
+    break;
+  case "*":
+    result = numOne * numTwo;
+    break;
+  case "/":
+    result = numOne / numTwo;
+    break;
+  case "%":
+    result = numOne % numTwo;
+    break;
+}
+
+console.log(`Result: ${numOne} ${operator} ${numTwo} = ${result}`);
